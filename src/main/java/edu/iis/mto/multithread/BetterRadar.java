3e28a987cb8d54760cbaedec43b83dc23c8e7684
Daniel Stanczak <daniel_stanczak@hotmail.com>
2015-04-27 11:59:50 +0200

    Dodanie klasy RepeatRule.java w celu możliwości wykonania testu BetterRadarTest.java zadaną ilość razy

--- src/main/java/edu/iis/mto/multithread/BetterRadar.java
+++ src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -3,9 +3,11 @@ package edu.iis.mto.multithread;
 import java.util.concurrent.Executor;
 
 public class BetterRadar {
+	
 	private PatriotBattery battery;
 	private Executor exec;
 
+	
 	public Executor getExec() {
 		return exec;
 	}
--- src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -5,8 +5,17 @@ import static org.mockito.Mockito.verify;
 
 import java.util.concurrent.Executor;
 
+import org.junit.Rule;
+import org.junit.Test;
+import edu.iis.mto.multithread.RepeatRule.Repeat;
+
 public class BetterRadarTest {
 
+	@Rule
+	public RepeatRule repeatRule = new RepeatRule();
+
+	@Repeat(times = 10)
+	@Test
 	public void launchPatriotOnceWhenNoticesAScudMissle() {
 		PatriotBattery batteryMock = mock(PatriotBattery.class);
 		BetterRadar radar = new BetterRadar(batteryMock);
